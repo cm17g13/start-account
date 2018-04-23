9668b44a22e91817f501ad32f274961e259eeb8e
cm17g13 <callummcgregor50@gmail.com>
2018-04-23 15:02:27 +0100

    Insert data inso sql table at startup

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO Accounts(firstName, secondName, accountNumber) VALUES ('John', 'Doe', '1234');
+INSERT INTO Accounts(firstName, secondName, accountNumber) VALUES ('Jane', 'Doe', '1235');
+INSERT INTO Accounts(firstName, secondName, accountNumber) VALUES ('Jim', 'Taylor', '1236');
+
