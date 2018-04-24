6a6f0cf869c5e2fc222508dfde19066765202cf5
cm17g13 <callummcgregor50@gmail.com>
2018-04-24 18:42:25 +0100

    Done creating test suite

--- src/main/java/com/qa/service/AccountManagerDBImpl.java
+++ src/main/java/com/qa/service/AccountManagerDBImpl.java
@@ -12,25 +12,23 @@ import com.qa.util.JSONUtil;
 public class AccountManagerDBImpl {
 
 	@Inject
-	JSONUtil jsonUtil;
+	private JSONUtil jsonConverter;
 
 	@PersistenceContext(unitName = "primary")
 	private EntityManager manager;
 
-	@SuppressWarnings("unchecked")
 	public String getAllAccounts() {
 		
-		Query query = manager.createQuery("SELECT a FROM ACCOUNTS a");
-		Collection<Account> accounts = (Collection<Account>) query.getResultList();
-		return jsonUtil.getJSONForObject(accounts);
+		TypedQuery<Account> query = manager.createQuery("SELECT a FROM ACCOUNTS a", Account.class);
+		Collection<Account> accounts = query.getResultList();
+		return jsonConverter.getJSONForObject(accounts);
 	}
 	
-	
-	@Transactional(SUPPORTS)
-	private Account findAnAccount(String accountNumber) {
-		
-		Query query = manager.createQuery("SELECT a FROM ACCOUNTS a WHERE a.accountNumber == :accountNumber", Account.class);
-		return  (Account) query.getSingleResult();
+
+	public Account findAnAccount(String accountNumber) {
+
+		TypedQuery<Account> query = manager.createQuery("SELECT a FROM ACCOUNTS a WHERE a.accountNumber = :accountNumber", Account.class);
+		return   query.getSingleResult();
 		//return manager.find(Account.class, accountNumber); how you'd do it if you want to use find in the EntityManager
 	}
 	
@@ -38,28 +36,26 @@ public class AccountManagerDBImpl {
 	@Transactional(REQUIRED)
 	public String createAccount(String account) {
 		
-		Account newAccount = jsonUtil.getObjectForJSON(account, Account.class);
+		Account newAccount = jsonConverter.getObjectForJSON(account, Account.class);
 		if(findAnAccount(newAccount.getAccountNumber()) != null) {
 			return "{\"message\": \"the account exists, and so was not added\"}";
 		} else {
 			manager.persist(newAccount);
 			return "{\"message\": \"the account has been added\"}";
 		}
-		
 	}
 	
 	
 	@Transactional(REQUIRED)
 	public String updateAccount(String account) {
 		
-		Account existingAccount = jsonUtil.getObjectForJSON(account, Account.class);
+		Account existingAccount = jsonConverter.getObjectForJSON(account, Account.class);
 		if (findAnAccount(existingAccount.getAccountNumber()) != null) {
 			manager.merge(existingAccount);
 			return "{\"message\": \"the account has been updated\"}";
 		} else {
 			return "{\"message\": \"the account did not exist, and so was not updated\"}";
 		}
-		
 	}
 	
 	@Transactional(REQUIRED)
@@ -72,6 +68,16 @@ public class AccountManagerDBImpl {
 		} else {
 			return "{\"message\": \"the account did not exist, and so was not deleted\"}";
 		}	
+	}
+	
+	@Transactional(REQUIRED)
+	public void setEntityManager(EntityManager manager) {
+		this.manager = manager;
+	}
+	
+	@Transactional(REQUIRED)
+	public void setJsonConverter(JSONUtil jsonConverter) {
+		this.jsonConverter = jsonConverter;
 	}
 }
 
---
+++ src/test/java/com/qa/service/AccountManagerTest.java
@@ -0,0 +1,81 @@
+package com.qa.service;
+
+import com.qa.domain.Account;
+import com.qa.util.JSONUtil;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.ArrayList;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AccountManagerTest {
+	
+	@InjectMocks
+	private AccountManagerDBImpl accountManager;
+
+    private JSONUtil jsonConverter;
+	
+	@Mock
+    private EntityManager manager;
+
+	@Mock
+    private TypedQuery<Account> query;
+
+	String person1 = "{\"firstName\":\"Tib\",\"secondName\":\"Coder\",\"accountNumber\":\"1111\"}";
+	
+	@Before
+	public void before() {
+		accountManager.setEntityManager(manager);
+		this.jsonConverter = new JSONUtil();
+		accountManager.setJsonConverter(jsonConverter);
+	}
+
+	@Test
+	public void getAllAccountsTest() {
+        Mockito.when(manager.createQuery(Mockito.anyString(), Mockito.eq(Account.class))).thenReturn(query);
+        ArrayList<Account> accounts = new ArrayList<Account>();
+        accounts.add(new Account("Tib", "Coder", "1111"));
+        Mockito.when(query.getResultList()).thenReturn(accounts);
+        String personArray = "[" + person1 + "]";
+        Assert.assertEquals(personArray, accountManager.getAllAccounts());
+	}
+	
+
+	@Test
+	public void findAnAccountTest() {
+        Mockito.when(manager.createQuery(Mockito.anyString(), Mockito.eq(Account.class))).thenReturn(query);
+        Mockito.when(query.getSingleResult()).thenReturn(new Account("Tib", "Coder", "1111"));
+        Assert.assertEquals(person1, jsonConverter.getJSONForObject(accountManager.findAnAccount("1111")));
+	}
+
+	@Test
+	public void createAccountTest() {
+        Mockito.when(manager.createQuery(Mockito.anyString(), Mockito.eq(Account.class))).thenReturn(query);
+        Mockito.when(query.getSingleResult()).thenReturn(null);
+        Assert.assertEquals("{\"message\": \"the account has been added\"}", accountManager.createAccount(person1));
+    }
+
+	@Test
+	public void updateAccountTest() {
+        Mockito.when(manager.createQuery(Mockito.anyString(), Mockito.eq(Account.class))).thenReturn(query);
+        Mockito.when(query.getSingleResult()).thenReturn(new Account("Tib", "Coder", "1111"));
+        Assert.assertEquals("{\"message\": \"the account has been updated\"}", accountManager.updateAccount(person1));
+
+	}
+
+	@Test
+	public void deleteAccountTest() {
+        Mockito.when(manager.createQuery(Mockito.anyString(), Mockito.eq(Account.class))).thenReturn(query);
+        Mockito.when(query.getSingleResult()).thenReturn(new Account("Tib", "Coder", "1111"));
+        Assert.assertEquals("{\"message\": \"the account has been deleted\"}", accountManager.deleteAccount(person1));
+
+    }
+}
